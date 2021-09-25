14ec172089bb74f1669059688166c94615d738a4
Bucurenciu Bogdan Cristian <bucurenciu.cristian@gmail.com>
2021-09-25 13:15:25 +0300

    part 1c is done.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,60 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Header = (props) => {
-    console.log(props);
+const Display = ({counter}) => <div>{counter}</div>;
+const Button = ({onClick, text}) => {
     return (
-        <h1>{props.course}</h1>);
-}
-const Part = (props) => {
-    console.log(props);
-
-    return (<p>
-        {props.part} {props.exercise}
-    </p>);
-};
-const Content = (props) => {
-    console.log(props);
-
-    return (
-        <div>
-            <Part part={props.parts[0].name} exercise={props.parts[0].exercises}/>
-            <Part part={props.parts[1].name} exercise={props.parts[1].exercises}/>
-            <Part part={props.parts[2].name} exercise={props.parts[2].exercises}/>
-        </div>
-    );
-}
-const Total = (props) => {
-    console.log(props);
-
-    return (
-        <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    );
+        <button onClick={onClick}>
+            {text}
+        </button>
+    )
 }
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ];
+    const [counter, setCounter] = useState(0)
 
 
+    const setToZero = () => setCounter(0);
+    const increaseByOne = () => setCounter(counter + 1);
+    const decreaseByOne = () => setCounter(counter - 1)
     return (
-        <div>
-            <Header course={course}/>
-            <Content parts={parts}/>
-            <Total parts={parts}/>
-        </div>
-    )
+        <>
+            <Display counter={counter}/>
+            <Button onClick={increaseByOne} text='plus'/>
+            <Button onClick={setToZero} text='zero'/>
+            <Button onClick={decreaseByOne} text='minus'/>
+        </>
+    );
 }
 
 export default App
